acc224811a0977ff9f63d345e115d36f7c5f05cb
Griffin Wilchuk <77641980+GriffWilchuk@users.noreply.github.com>
2022-02-22 13:51:44 -0800

    Update Main.java
    Added basic infinite scanner, user entry is labeled "input".

--- ChatBot Project/Main.java
+++ ChatBot Project/Main.java
@@ -1,8 +1,19 @@
-
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-        // This is an example of how to access a movie that ryan reynolds is in
+       This is an example of how to access a movie that ryan reynolds is in
         RyanReynolds r = new RyanReynolds();
         System.out.println("Hello, this is an example: \n" + r.getDeadpool().toString());
+
+
+        Scanner in = new Scanner(System.in);
+        String input = "";        //initialize string
+        while(input != "End"){    // loop will run till user enters "End" (change to something nicer later on)
+          input = in.nextLine();  //string input is updated to users entry
+
+            }
+
+
+
     }
 }
