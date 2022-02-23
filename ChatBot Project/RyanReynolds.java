abe367dd9fa0cf3b9bde742739329a4e9effd6e6
Lance Rogan <77311869+lance1ca@users.noreply.github.com>
2022-02-22 19:05:48 -0800

    Changing IMDb stars to double

--- ChatBot Project/RyanReynolds.java
+++ ChatBot Project/RyanReynolds.java
@@ -24,6 +24,14 @@ public class RyanReynolds {
 
 
     //Deadpool 2
+    ArrayList<String> deadpool2Cast = new ArrayList<>(Arrays.asList("Me","Morena Baccarin", " T.J. Miller","Ed Skrein","Karan Soni","Michael Benyaer","Stefan Kapicic", "Brianna Hildebrand", "Style Dayne","Kyle Cassie", "Taylor Hickson", "Ayzee", "Naika Toussaint", "Randal Reeder", "Isaac C. Singleton Jr.", "Justyn Shippelt","Donna Tamamotot", "Jed Rees"));
+    ArrayList<String> deadpool2Awards = new ArrayList<>(Arrays.asList("Saturn Award","All Def Movie Award","ACCA","Critics Choice Award", "Artios Award","DFCS Award","Dragon Award","Empire Award","Golden Schmoes","Golden Trailer","HFCS Award","IGN People's Choice Award","MTV Movie Award","OFTA Film Award","Maxwell Weinberg Award","Teen Choice Award"));
+    private Movie deadpool2 = new Movie(7.7,"Deadpool",2016,"R",deadpoolCast,"Tim Miller","Action and Comedy",deadpoolAwards,"783,100,000");
+
+    //getter for deadpool movie
+    public Movie getDeadpool2(){
+        return deadpool2;
+    }
 
 
     //Free guy
