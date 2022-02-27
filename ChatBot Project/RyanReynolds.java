e53b2cb026e6645536727a238be9d65ed5bb5f69
Ben Van Bergeyk <bennymvanb@gmail.com>
2022-02-26 18:03:37 -0800

    updated my changes from main branch

--- ChatBot Project/RyanReynolds.java
+++ ChatBot Project/RyanReynolds.java
@@ -15,6 +15,28 @@ public class RyanReynolds {
     //gender
     private String gender;
 
+  // Information below
+    //birth place
+    private String birthPlace;
+    //birth date
+    private String birthDate;
+    //wife
+    private String wifeName;
+    //social media handle
+    private String socialMediaHandle;
+    //twitter following
+    private int twitterFollowers;
+    //instagram following
+    private int instagramFollowers;
+    //tiktok following
+    private int tiktokFollowers;
+    //Net Worth
+    private String netWorth;
+    //previous marriage
+    private String previousMarriage;
+    //other awards
+    private ArrayList<String> otherAwards;
+
 
 
     // ****IN PROGRESS BELOW, NOT FINISHED***
@@ -34,7 +56,7 @@ public class RyanReynolds {
     }
 
     //Deadpool 2
-    ArrayList<String> deadpool2Cast = new ArrayList<>(Arrays.asList("Me", "Josh Brolin", "Morena Baccarin","Julian Dennison", "Zazie Beetz", " T.J. Miller", "Leslie Uggams", "Karan Soni","Brianna Hildebrand","Jack Kesy", "Eddie Marsan", "Shioli Kutsuna","Stefan Kapicic", "Randal Reeder", "Nikolai Witschl", "Thayr Harris", "Rob Delaney", "Lewis Tan"));
+    ArrayList<String> deadpool2Cast = new ArrayList<>(Arrays.asList("Me", "Josh Brolin", "Morena Baccarin","Julian Dennison", "Zazie Beetz", "T.J. Miller", "Leslie Uggams", "Karan Soni","Brianna Hildebrand","Jack Kesy", "Eddie Marsan", "Shioli Kutsuna","Stefan Kapicic", "Randal Reeder", "Nikolai Witschl", "Thayr Harris", "Rob Delaney", "Lewis Tan"));
     ArrayList<String> deadpool2Awards = new ArrayList<>(Arrays.asList("Golden Schmoes", "Golden Trailer", "Aurora Award"));
     private Movie deadpool2 = new Movie(7.7,"Deadpool 2",2018,"R",deadpool2Cast,"David Leitch","Action and Comedy",deadpool2Awards,"785,000,000");
 
@@ -64,43 +86,43 @@ public class RyanReynolds {
         return ripd;
     }
 
-    // Getting groups opinion for the rest of detail below: 
-    //Green Lantern**
-    ArrayList<String> greenLaternCast = new ArrayList<>(Arrays.asList("Me", "Josh Brolin", "Morena Baccarin","Julian Dennison", "Zazie Beetz", " T.J. Miller", "Leslie Uggams", "Karan Soni","Brianna Hildebrand","Jack Kesy", "Eddie Marsan", "Shioli Kutsuna","Stefan Kapicic", "Randal Reeder", "Nikolai Witschl", "Thayr Harris", "Rob Delaney", "Lewis Tan"));
-    ArrayList<String> greenLaternAwards = new ArrayList<>(Arrays.asList("Golden Schmoes", "Golden Trailer", "Aurora Award"));
-    private Movie greenLatern = new Movie(7.7,"Deadpool 2",2018,"R",deadpool2Cast,"David Leitch","Action and Comedy",deadpool2Awards,"785,000,000");
+  
+    //Green Lantern
+    ArrayList<String> greenLanternCast = new ArrayList<>(Arrays.asList("Me", "Blake Lively", "Peter Sarsgaard","Mark Strong", "Tim Robbins", "Jay O. Sanders", "Taika Waititi", "Angela Bassett","Mike Doyle","Nick Jandl", "Dylan James", "Gattlin Griffith","Jon Tenney", "Leanne Cochran", "Temuera Morrison", "Jeff Wolfe", "Lena Clark", "Jenna Craig"));
+    ArrayList<String> greenLanternAwards = new ArrayList<>(Arrays.asList("ASCAP Award", "Golden Schmoes", "HPA Awards"));
+    private Movie greenLantern = new Movie(5.5,"Green Lantern",2011,"PG-13",greenLanternCast,"Martin Campbell","Action and Sci-Fi",greenLanternAwards,"219,900,000");
 
-    //getter for green lantern movie**
-    public Movie getGreenLatern(){
-        return greenLatern;
+    //getter for green lantern movie
+    public Movie getGreenLantern(){
+        return greenLantern;
     }
 
-    //Buried**
-    ArrayList<String> buriedCast = new ArrayList<>(Arrays.asList("Me", "Josh Brolin", "Morena Baccarin","Julian Dennison", "Zazie Beetz", " T.J. Miller", "Leslie Uggams", "Karan Soni","Brianna Hildebrand","Jack Kesy", "Eddie Marsan", "Shioli Kutsuna","Stefan Kapicic", "Randal Reeder", "Nikolai Witschl", "Thayr Harris", "Rob Delaney", "Lewis Tan"));
-    ArrayList<String> buriedAwards = new ArrayList<>(Arrays.asList("Golden Schmoes", "Golden Trailer", "Aurora Award"));
-    private Movie buried = new Movie(7.7,"Deadpool 2",2018,"R",deadpool2Cast,"David Leitch","Action and Comedy",deadpool2Awards,"785,000,000");
+    //Buried
+    ArrayList<String> buriedCast = new ArrayList<>(Arrays.asList("Me", "Jose Luis Garcia Perez", "Robert Paterson","Stephen Tobolowsky", "Samantha Mathis", "Ivana Mino", "Warner Loughlin", "Erik Palladino","Kali Rocha","Chris William Martin", "Cade Dundish", "Mary Birdsong","Kirk Baily", "Anne Lockhart", "Robert Clotworthy", "Mitchalla Petersen", "Juan Hidalgo", "Abdelilah Ben Massou"));
+    ArrayList<String> buriedAwards = new ArrayList<>(Arrays.asList("ASECAN Award", "Bronze Frog", "CEC Award", "Critics Award","Chainsaw Award","Prix Nouveau Genre", "Gaudi Award","Goya", "Jose Maria Forque Award","NBR Award","Georges Award","Sant Jordi","Grand Prize of European Fantasy Film in Gold"));
+    private Movie buried = new Movie(7.0,"Buried",2010,"R",buriedCast,"Rodrigo Cortes","Drama, Mystery, and Thriller",buriedAwards,"21,300,000");
 
-    //getter for buried movie**
+    //getter for buried movie
     public Movie getBuried(){
         return buried;
     }
 
-    //6 Underground**
-    ArrayList<String> sixUndergroundCast = new ArrayList<>(Arrays.asList("Me", "Josh Brolin", "Morena Baccarin","Julian Dennison", "Zazie Beetz", " T.J. Miller", "Leslie Uggams", "Karan Soni","Brianna Hildebrand","Jack Kesy", "Eddie Marsan", "Shioli Kutsuna","Stefan Kapicic", "Randal Reeder", "Nikolai Witschl", "Thayr Harris", "Rob Delaney", "Lewis Tan"));
-    ArrayList<String> sixUndergroundAwards = new ArrayList<>(Arrays.asList("Golden Schmoes", "Golden Trailer", "Aurora Award"));
-    private Movie sixUnderground = new Movie(7.7,"Deadpool 2",2018,"R",deadpool2Cast,"David Leitch","Action and Comedy",deadpool2Awards,"785,000,000");
+    //6 Underground
+    ArrayList<String> sixUndergroundCast = new ArrayList<>(Arrays.asList("Me", "Melanie Laurent", "Manuel Garcia-Rulfo","Ben Hardy", "Adira Arjona", "Dave Franco", "Corey Hawkins", "Lior Raz","Payman Maadi","Yuri Kolokolnikov", "Kim Kold", "Lidia Franco","James Murray", "Lukhanyo Bele", "George Kareman", "James Carroll Jordan", "Ron Funches", "Daniel Adegboyega"));
+    ArrayList<String> sixUndergroundAwards = new ArrayList<>(Arrays.asList("Taurus World Stunt Award"));
+    private Movie sixUnderground = new Movie(6.1,"6 Underground",2019,"R",sixUndergroundCast,"Michael Bay","Action and Thriller",sixUndergroundAwards,"N/A");
 
-    //getter for 6 Underground movie**
+    //getter for 6 Underground movie
     public Movie getSixUnderground(){
         return sixUnderground;
     }
 
-    //Red Notice**
-    ArrayList<String> redNoticeCast = new ArrayList<>(Arrays.asList("Me", "Josh Brolin", "Morena Baccarin","Julian Dennison", "Zazie Beetz", " T.J. Miller", "Leslie Uggams", "Karan Soni","Brianna Hildebrand","Jack Kesy", "Eddie Marsan", "Shioli Kutsuna","Stefan Kapicic", "Randal Reeder", "Nikolai Witschl", "Thayr Harris", "Rob Delaney", "Lewis Tan"));
-    ArrayList<String> redNoticeAwards = new ArrayList<>(Arrays.asList("Golden Schmoes", "Golden Trailer", "Aurora Award"));
-    private Movie redNotice = new Movie(7.7,"Deadpool 2",2018,"R",deadpool2Cast,"David Leitch","Action and Comedy",deadpool2Awards,"785,000,000");
+    //Red Notice
+    ArrayList<String> redNoticeCast = new ArrayList<>(Arrays.asList("Me", "Dwayne Johnson", "Gal Gadot","Ritu Arya", "Chris Diamantopoulos", "Ivan Mbakop", "Vincenzo Amato", "Rafael Petardi","Seth Michaels","Sebastien Large", "Guy Nardulli", "Andrew Hunter","George Tsai", "Rawson Marshall Thurber", "Robert Mata", "Anthony Belevtsov", "Daniel Bernhardt", "Yosef Podolski"));
+    ArrayList<String> redNoticeAwards = new ArrayList<>(Arrays.asList("N/A"));
+    private Movie redNotice = new Movie(6.3,"Red Notice",2021,"PG-13",redNoticeCast,"Rawson Marshall Thurber","Action, Comedy, and Thriller",redNoticeAwards,"2,000,000");
 
-    //getter for red notice movie**
+    //getter for red notice movie
     public Movie getRedNotice(){
         return redNotice;
     }
@@ -159,10 +181,38 @@ public class RyanReynolds {
         this.gender = gender;
     }
 
-    RyanReynolds(){
-        
+    //RyanReynolds Constructor for all personal information
+
+    RyanReynolds(String birthPlace, String birthDate,String wifeName,String socialMediaHandle, String netWorth, String previousMarriage, ArrayList<String> otherAwards){
+        this.birthPlace = birthPlace;
+        this.birthDate = birthDate;
+        this.wifeName = wifeName;
+        this.haircolour = socialMediaHandle;
+        this.netWorth = netWorth;
+        this.previousMarriage = previousMarriage;
+        this.otherAwards = otherAwards;
     }
 
+    //RyanReynolds Constructor for all physical attributes and personal information (i.e, full RyanReynolds constructor)
+
+    RyanReynolds(String height, int weight,String eyecolour,String haircolour, String gender,String birthPlace, String birthDate,String wifeName,String socialMediaHandle, String netWorth, String previousMarriage, ArrayList<String> otherAwards){
+        this.height = height;
+        this.weight = weight;
+        this.eyecolour = eyecolour;
+        this.haircolour = haircolour;
+        this.gender = gender;
+        this.birthPlace = birthPlace;
+        this.birthDate = birthDate;
+        this.wifeName = wifeName;
+        this.haircolour = socialMediaHandle;
+        this.netWorth = netWorth;
+        this.previousMarriage = previousMarriage;
+        this.otherAwards = otherAwards;
+    }
+
+    RyanReynolds(){
+
+    }
 
     //getters and setters below
     public String getheight(){
@@ -197,5 +247,86 @@ public class RyanReynolds {
         this.gender = gender;
     }
 
+    //getters and setters for information
+    public String getbirthPlace(){
+        return birthPlace;
+    }
+
+    public void setbirthPlace(String birthPlace){
+        this.birthPlace = birthPlace;
+    }
+
+    public String getbirthDate(){
+        return birthDate;
+    }
+
+    public void setbirthDate(String birthDate){
+        this.birthDate = birthDate;
+    }
+
+    public String getwifeName(){
+        return wifeName;
+    }
+
+    public void setwifeName(String wifeName){
+        this.wifeName = wifeName;
+    }
+
+    public String getsocialMediaHandle(){
+        return socialMediaHandle;
+    }
+
+    public void setsocialMediaHandle(String socialMediaHandle){
+        this.socialMediaHandle = socialMediaHandle;
+    }
+
+    public int gettwitterFollowers(){
+        return twitterFollowers;
+    }
+
+    public void setinstagramFollowers(int instagramFollowers){
+        this.instagramFollowers = instagramFollowers;
+    }
+
+    public int getinstagramFollowers(){
+        return instagramFollowers;
+    }
+
+    public void settiktokFollowers(int tiktokFollowers){
+        this.tiktokFollowers = tiktokFollowers;
+    }
+
+    public int gettiktokFollowers(){
+        return tiktokFollowers;
+    }
+
+    public void settwitterFollowers(int twitterFollowers){
+        this.twitterFollowers = twitterFollowers;
+    }
+
+
+    public String getnetWorth(){
+        return netWorth;
+    }
+
+    public void setnetWorth(String netWorth){
+        this.netWorth = netWorth;
+    }
+
+    public String getpreviousMarriage(){
+        return previousMarriage;
+    }
+
+    public void setpreviousMarriageh(String previousMarriage){
+        this.previousMarriage = previousMarriage;
+    }
+
+    public ArrayList<String> getotherAwards(){
+        return otherAwards;
+    }
+
+    public void setotherAwards(ArrayList<String> otherAwards){
+        this.otherAwards = otherAwards;
+    }
 
 }
