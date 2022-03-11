263d11da7b997b4587ed5bd4bc5f168091745f80
Lance Rogan <77311869+lance1ca@users.noreply.github.com>
2022-03-10 16:27:20 -0800

    Working version for ONLY movie questions

--- ChatBot Project/chatBotTest.java
+++ ChatBot Project/chatBotTest.java
@@ -34,19 +34,19 @@ public class chatBotTest {
 //testMap
 
      //Initializing the imdb map
-     static HashMap<String, Double> imdbMap = new HashMap<String, Double>();
+     static HashMap<String, String> imdbMap = new HashMap<String, String>();
     //Initializing the year map
-    static HashMap<String, Integer> yearMap = new HashMap<String, Integer>();
+    static HashMap<String, String> yearMap = new HashMap<String, String>();
     //Initializing the rating map
     static HashMap<String, String> ratingMap = new HashMap<String, String>();
     //Initializing the cast map
-    static HashMap<String, ArrayList> castMap = new HashMap<String, ArrayList>();
+    static HashMap<String, String> castMap = new HashMap<String, String>();
     //Initializing the director map
     static HashMap<String, String> directorMap = new HashMap<String, String>();
     //Initializing the genre map
     static HashMap<String, String> genreMap = new HashMap<String, String>();
     //Initializing the awards map
-    static HashMap<String, ArrayList> awardsMap = new HashMap<String, ArrayList>();
+    static HashMap<String, String> awardsMap = new HashMap<String, String>();
     //Initializing the box office map
     static HashMap<String, String> boxOfficeMap = new HashMap<String, String>();
     //Initializing the location map
@@ -186,54 +186,54 @@ public class chatBotTest {
 
   // this is a method which fills in each map with its respective key and value depending on which map it is
   // via a process of elimination by if and else which determines what the key will be 
-  public static <T> void fillInMap(HashMap<String,String> map,String value){
+  public static void fillInMap(HashMap<String,String> map,String value){
     
     for(int i=0; i<listOfMovies.size();i++){
 
       if(value == "imdb"){
-        map.put(listOfMovies.get(i).getTitle()," has " + listOfMovies.get(i).getImdbStars()+" IMDb stars!");
+        map.put(listOfMovies.get(i).getTitle().toLowerCase()," has " + listOfMovies.get(i).getImdbStars()+" IMDb stars!");
 
       }else if(value =="year"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getYear());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase()," was released in " + listOfMovies.get(i).getYear());
 
       }else if(value =="rating"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getRating());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase()," is rated " + listOfMovies.get(i).getRating());
 
       }
       else if(value =="cast"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getCast());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase(),"'s cast includes: " +listOfMovies.get(i).getCast());
 
       }
       else if(value =="director"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getDirector());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase(),"'s director was: "+listOfMovies.get(i).getDirector());
 
       }
       else if(value =="genre"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getGenre());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase(),"'s genre is " + listOfMovies.get(i).getGenre());
 
       }
       else if(value =="awards"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getAwards());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase()," has won these awards: " +listOfMovies.get(i).getAwards());
 
       }
       else if(value =="box office"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getBoxOffice());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase(),"'s box office profit is " +listOfMovies.get(i).getBoxOffice());
 
       }
       else if(value =="location"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getLocation());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase(),"'s filming location was " + listOfMovies.get(i).getLocation());
 
       }
       else if(value =="time"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getTimeToFilm());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase(),"'s time taken to film was " + listOfMovies.get(i).getTimeToFilm());
 
       }
       else if(value =="duration"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getDuration());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase()," is " + listOfMovies.get(i).getDuration() + " long");
 
       }
       else if(value =="budget"){
-        map.put(listOfMovies.get(i).getTitle(),listOfMovies.get(i).getBudgetOfMovie());
+        map.put(listOfMovies.get(i).getTitle().toLowerCase()," had a budget of " + listOfMovies.get(i).getBudgetOfMovie());
 
       }else{
 
@@ -260,8 +260,8 @@ public class chatBotTest {
 
    // if the userInput contains a movie title, we segregate ithe users query to being about movies
    for(int i=0; i<listOfMovies.size();i++){
-       if(userInput.contains(listOfMovies.get(i).getTitle())){
-           movieTitleAsked = listOfMovies.get(i).getTitle();
+       if(userInput.contains(listOfMovies.get(i).getTitle().toLowerCase())){
+           movieTitleAsked = listOfMovies.get(i).getTitle().toLowerCase();
            
            //then we send the user input and the movie title asked about to a chat function which determines
            // the robots reponse
@@ -280,12 +280,65 @@ public class chatBotTest {
 
   public static void movieChatFunction(String userInput, String movieTitleAsked){
 
-    for(int i=0; i<movieQuestion.size();i++){
+    
       // if the user input contains one of the possible movie questions key word, we respond appropriately
-        if(userInput.contains(movieQuestion.get(i))){
-            System.out.println(movieTitleAsked + imdbMap.get(movieTitleAsked));
+      for(int i=0; i<movieQuestion.size();i++){
+          if(userInput.contains(movieQuestion.get(0))){
+           
+            System.out.println(movieTitleAsked +" " +imdbMap.get(movieTitleAsked));
+            break;
+    
+          }else if(userInput.contains(movieQuestion.get(1))){
+            System.out.println(movieTitleAsked +" " + yearMap.get(movieTitleAsked));
+            break;
+
+          }else if(userInput.contains(movieQuestion.get(2))){
+            System.out.println(movieTitleAsked +" " + imdbMap.get(movieTitleAsked));
+            break;
+            
+          }
+          else if(userInput.contains(movieQuestion.get(3))){
+            System.out.println(movieTitleAsked +" " + castMap.get(movieTitleAsked));
+            break;
+          }
+          else if(userInput.contains(movieQuestion.get(4))){
+            System.out.println(movieTitleAsked +" " + directorMap.get(movieTitleAsked));
+            break;
+          }
+          else if(userInput.contains(movieQuestion.get(5))){
+            System.out.println(movieTitleAsked +" " + genreMap.get(movieTitleAsked));
+            break;
+          }
+          else if(userInput.contains(movieQuestion.get(6))){
+            System.out.println(movieTitleAsked +" " + awardsMap.get(movieTitleAsked));
+            break;
+          }
+          else if(userInput.contains(movieQuestion.get(7))){
+            System.out.println(movieTitleAsked +" " + boxOfficeMap.get(movieTitleAsked));
+            break;
+          }
+          else if(userInput.contains(movieQuestion.get(8))){
+            System.out.println(movieTitleAsked +" " + locationMap.get(movieTitleAsked));
+            break;
+          }
+          else if(userInput.contains(movieQuestion.get(9))){
+            System.out.println(movieTitleAsked +" " + timeToFilmMap.get(movieTitleAsked));
+            break;
+          }
+          else if(userInput.contains(movieQuestion.get(10))){
+            System.out.println(movieTitleAsked +" " + durationMap.get(movieTitleAsked));
+            break;
+          }
+          else if(userInput.contains(movieQuestion.get(11))){
+            System.out.println(movieTitleAsked +" " + budgetMap.get(movieTitleAsked));
+            break;
+          }else{
+    
+            return;
+          }
         }
+        
     }
   }
 
-}
+
