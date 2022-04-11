2fc454cf0530c566d805aa3ed537aed6eb1e6fe7
Ben Van Bergeyk <bennymvanb@gmail.com>
2022-04-11 11:42:26 -0700

    removed unused imports

--- ChatBot A#3 Project/chatbot/src/main/java/group10/ChatBot.java
+++ ChatBot A#3 Project/chatbot/src/main/java/group10/ChatBot.java
@@ -27,11 +27,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 //----------------------------------------------------------------------------------------------------------------------------
 // our class ChatBot which extends JFrame and implements the action listener
